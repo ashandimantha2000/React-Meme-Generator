0d7041e6d84dcf02a68ef95b8fe2949d0e552a1e
Ashan <ashandimanthalk@gmail.com>
2023-11-13 15:36:24 +0530

    added nav

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import TrollFace from '../assets/images/Troll Face.png'
+
+function Header() {
+  return (
+    <header className='header'>
+        <img src={TrollFace} alt="icon" className='header-image'/>
+        <h2 className='header-title'>Meme Generator</h2>
+        <h4 className='header-subtitle'>Image Manipulator</h4>
+    </header>
+  )
+}
+
+export default Header
